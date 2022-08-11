b23925206d7d58fbbd75ca9cc24391eb881a8af3
mateuzor <mateuzor@gmail.com>
2022-08-11 12:00:53 -0300

    solution refactoring

--- birthday-email/Solution.js
+++ birthday-email/Solution.js
@@ -1,26 +1,21 @@
 var fs = require("fs");
-
 class Solution {
   email(birthday) {
-    var text = fs.readFileSync(
-      "C:\\Users\\mateu\\POCS\\algo-exercises\\birthday-email\\friends.txt",
-      "utf8"
-    );
-
     if (birthday === "") return [];
 
-    const friendsArray = text.split("\r");
-    let friendsGreeted = [];
+    var text = fs.readFileSync(require.resolve("./friends.txt"), "utf8");
 
-    friendsArray.forEach((friend) => {
+    const friendsArray = text.split("\n");
+
+    return friendsArray.reduce((greetings, friend) => {
       const friendBirthDay = friend.split(",")[2].trim();
 
       if (birthday === friendBirthDay) {
         const friendName = friend.split(",")[1].trim();
-        friendsGreeted.push(`Happy birthday, dear ${friendName}!`);
+        greetings.push(`Happy birthday, dear ${friendName}!`);
       }
-    });
-    return friendsGreeted;
+      return greetings;
+    }, []);
   }
 }
 
