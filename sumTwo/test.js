3e75740bd7144eb6c67f8eafb8b2f85afb26bb9b
mateuzor <mateuzor@gmail.com>
2022-08-24 20:58:42 -0300

    changed test case name

--- sumTwo/test.js
+++ sumTwo/test.js
@@ -1,7 +1,7 @@
 const Solution = require("./Solution");
 const solution = new Solution();
 
-test("test", () => {
+test("test sum of two values match target", () => {
   const nums = [2, 7, 11, 15];
   const output = [1, 0];
   const target = 9;
