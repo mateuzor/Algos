9e7d5a99fa823ca2e0e67b25d0a101b2b3ff2fff
mateuzor <mateuzor@gmail.com>
2022-09-01 14:05:24 -0300

    removed consoles

--- group-anagrams/Solution.js
+++ group-anagrams/Solution.js
@@ -2,11 +2,9 @@ class Solution {
   groupAnagrams(strs) {
     let hash = {};
     for (let i = 0; i < strs.length; i++) {
-      console.log(hash);
       let word = strs[i].split("").sort().join("");
       hash[word] = hash[word] || [];
       hash[word].push(strs[i]);
-      console.log(hash);
     }
     return Object.values(hash);
   }
